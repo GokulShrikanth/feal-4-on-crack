465af2a11da2d78ddec64b6d0a865ddad88bee14
Gokul Shrikanth <gokulshrikanth99@gmail.com>
2023-11-17 15:30:12 +0000

    inital commit

---
+++ src/main/java/com/blaze/feal4.java
@@ -0,0 +1,34 @@
+package com.blaze;
+
+import io.vertx.core.Context;
+import io.vertx.core.Promise;
+import io.vertx.core.Verticle;
+import io.vertx.core.Vertx;
+
+public class feal4 implements Verticle {
+
+    @Override
+    public Vertx getVertx() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVertx'");
+    }
+
+    @Override
+    public void init(Vertx vertx, Context context) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'init'");
+    }
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void stop(Promise<Void> stopPromise) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+
+}
