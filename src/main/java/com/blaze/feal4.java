c3c4a5c3616349d29db9c3925e0357d8b0e24199
Gokul Shrikanth <gokulshrikanth99@gmail.com>
2023-11-19 20:02:54 +0000

    cleanup

--- src/main/java/com/blaze/feal4.java
+++ src/main/java/com/blaze/feal4.java
@@ -9,7 +9,6 @@ public class feal4 extends AbstractVerticle {
         System.out.println("Starting the decyption");
 
         for(int k1=0; k1<4096; k1++) {
-            System.out.println("Running intial loop for value :" + k1 + " of 4096");
             int keyTilda = solver.get12BitKeyForInnerBytes(k1);
             int firstA1 = solver.calcConstInnerBytesk0(0, keyTilda, input);
 
@@ -19,7 +18,6 @@ public class feal4 extends AbstractVerticle {
 
                 if(w1 == data.pairs-1) {
                     for(int k2=0; k2<1048576; k2++) {
-                        System.out.println("Running secondary loop for value :" + k2 + " of 1048576");
                         int key0 = solver.get20BitKeyForOutterBytes(k2, keyTilda);
                         int firstA2 = solver.calcConstOutteBytesk0(0, key0, input);
 
--- src/main/java/com/blaze/solver.java
+++ src/main/java/com/blaze/solver.java
@@ -57,15 +57,6 @@ public class solver {
         return (((k1 >> 6) & 0x3F) << 16) + ((k1 & 0x3F) << 8);
     }
 
-    public static int calcConstOutteBytesk0(int wordIndex, int key, data input) {
-        splitPairs(wordIndex, input);
-        int a1 = getBit(input.L0^input.R0^input.L4, 5)^getBit(input.L0^input.R0^input.L4, 13)^getBit(input.L0^input.R0^input.L4, 21);
-        int a2 = getBit(input.L0^input.L4^input.R4, 15);
-        int a3 = getBit(f(input.L0^input.R0^key), 15);
-
-        return a1^a2^a3;
-    }
-
     public static int get20BitKeyForOutterBytes(int k2, int keyTilda) {
         int a0 = (((k2 & 0xF) >> 2) << 6) + ((keyTilda >> 16) & 0xFF);
         int a1 = ((k2 & 0x3) << 6) + ((keyTilda >> 8) & 0xFF);
@@ -75,6 +66,15 @@ public class solver {
         int b2 = b3^a1;
 
         return (b0 << 24)  + (b1 << 16) + (b2 << 8) + b3;
+    }
+
+    public static int calcConstOutteBytesk0(int wordIndex, int key, data input) {
+        splitPairs(wordIndex, input);
+        int a1 = getBit(input.L0^input.R0^input.L4, 5)^getBit(input.L0^input.R0^input.L4, 13)^getBit(input.L0^input.R0^input.L4, 21);
+        int a2 = getBit(input.L0^input.L4^input.R4, 15);
+        int a3 = getBit(f(input.L0^input.R0^key), 15);
+
+        return a1^a2^a3;
     }
 
     public static int calcConstInnerBytesk0(int i, int keyTilda, data input) {
@@ -106,15 +106,15 @@ public class solver {
         return a1^a2;
     }
 
-    private static int generate20BitKeyForOutterBytes(int k, int key_tilda) {
-        int a0 = (((k & 0xF) >> 2) << 6) + ((key_tilda >> 16) & 0xFF);
-        int a1 = ((k & 0x3) << 6) + ((key_tilda >> 8) & 0xFF);
-        int b0 = (k >> 12) & 0xFF;
-        int b3 = (k >> 4) & 0xFF;
-        int b1 = b0^a0;
-        int b2 = b3^a1;
+    static int calcConstOutteBytesk2(int wordIndex, int k0, int k1, int k2, data input) {
+        splitPairs(wordIndex, input);
+        int a1 = getBit(input.L0^input.R0^input.L4, 13);
+        int y0 = f(input.L0^input.R0^k0);
+        int y1 = f(input.L0^y0^k1);
+        int y2 = f(input.L0^input.R0^y1^k2);
+        int a2 = getBit(y2, 7)^getBit(y2, 15)^getBit(y2, 23)^getBit(y2, 31);
 
-        return (b0 << 24)  + (b1 << 16) + (b2 << 8) + b3;
+        return a1^a2;
     }
 
     static int calcConstInnerBytesk2(int wordIndex, int key, int k0, int k1, data input) {
@@ -127,15 +127,17 @@ public class solver {
         return a1^a2;
     }
 
-    static int calcConstOutteBytesk2(int wordIndex, int k0, int k1, int k2, data input) {
+    private static int calConstOutteBytesk3(int wordIndex, int k0, int k1, int k2, int k3, data input) {
         splitPairs(wordIndex, input);
-        int a1 = getBit(input.L0^input.R0^input.L4, 13);
+        int a1 = getBit(input.L0^input.L4^input.R4, 13);
+        int a2 = getBit(input.L0^input.R0^input.L4, 7)^getBit(input.L0^input.R0^input.L4, 15)^getBit(input.L0^input.R0^input.L4, 23)^getBit(input.L0^input.R0^input.L4, 31);
         int y0 = f(input.L0^input.R0^k0);
         int y1 = f(input.L0^y0^k1);
         int y2 = f(input.L0^input.R0^y1^k2);
-        int a2 = getBit(y2, 7)^getBit(y2, 15)^getBit(y2, 23)^getBit(y2, 31);
+        int y3 = f(input.L0^y0^y2^k3);
+        int a3 = getBit(y3, 7)^getBit(y3, 15)^getBit(y3, 23)^getBit(y3, 31);
 
-        return a1^a2;
+        return a1^a2^a3;
     }
 
     static int calcConstInnerBytesk3(int wordIndex, int key, int k0, int k1, int k2, data input) {
@@ -146,20 +148,6 @@ public class solver {
         int y1 = f(input.L0^y0^k1);
         int y2 = f(input.L0^input.R0^y1^k2);
         int a3 = getBit(f(input.L0^y0^y2^key), 15);
-
-        return a1^a2^a3;
-    }
-
-
-    private static int calConstOutteBytesk3(int wordIndex, int k0, int k1, int k2, int k3, data input) {
-        splitPairs(wordIndex, input);
-        int a1 = getBit(input.L0^input.L4^input.R4, 13);
-        int a2 = getBit(input.L0^input.R0^input.L4, 7)^getBit(input.L0^input.R0^input.L4, 15)^getBit(input.L0^input.R0^input.L4, 23)^getBit(input.L0^input.R0^input.L4, 31);
-        int y0 = f(input.L0^input.R0^k0);
-        int y1 = f(input.L0^y0^k1);
-        int y2 = f(input.L0^input.R0^y1^k2);
-        int y3 = f(input.L0^y0^y2^k3);
-        int a3 = getBit(y3, 7)^getBit(y3, 15)^getBit(y3, 23)^getBit(y3, 31);
 
         return a1^a2^a3;
     }
@@ -175,7 +163,7 @@ public class solver {
 
                 if(w1 == data.pairs-1) {
                     for(int k2=0; k2<1048576; k2++) {
-                        int key1 = generate20BitKeyForOutterBytes(k2, key_tilda);
+                        int key1 = get20BitKeyForOutterBytes(k2, key_tilda);
                         int first_a2 = calcConstOutteBytesk1(0, k, key1, input);
 
                         for(int w2=1; w2<data.pairs; w2++) {
@@ -204,7 +192,7 @@ public class solver {
 
                 if(w1 == data.pairs-1) {
                     for(int k2=0; k2<1048576; k2++) {
-                        int key2 = generate20BitKeyForOutterBytes(k2, key_tilda);
+                        int key2 = get20BitKeyForOutterBytes(k2, key_tilda);
                         int first_a2 = calcConstOutteBytesk2(0, key0, key1, key2, input);
 
                         for(int w2=1; w2<data.pairs; w2++) {
@@ -233,7 +221,7 @@ public class solver {
 
                 if(w1 == data.pairs-1) {
                     for(int k2=0; k2<1048576; k2++) {
-                        int key3 = generate20BitKeyForOutterBytes(k2, key_tilda);
+                        int key3 = get20BitKeyForOutterBytes(k2, key_tilda);
                         int first_a2 = calConstOutteBytesk3(0, key0, key1, key2, key3, input);
 
                         for(int w2=1; w2<data.pairs; w2++) {
@@ -287,7 +275,7 @@ public class solver {
         System.out.print("\tK3 0x" + Integer.toHexString(key3));
         System.out.print("\tK4 0x" + Integer.toHexString(key4));
         System.out.println("\tK5 0x" + Integer.toHexString(key5));
-        System.out.println("************ Profit ************");
+        System.out.println("------------------- SOLVED --------------------");
     }
 
 }
